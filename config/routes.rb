e45dd739691d11d5ab125fc36030c87d2c9ed2e5
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-01-23 16:59:02 +0530

    Complete devise with successful redirect to dash

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   get 'static_pages/index'
 
