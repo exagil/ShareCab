ab6443fec6fa72580fbd3e1cc23eaab85d1c48c6
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-01-24 11:20:59 +0530

    Make gender field work and decide to switch to age rang

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
+      user.dob = auth.extra.raw_info.user_birthday
+      user.gender = auth.extra.raw_info.gender
     end
   end
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,5 +256,6 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, Constants::APP_ID, Constants::APP_SECRET
+  config.omniauth :facebook, Constants::APP_ID, Constants::APP_SECRET,
+           :scope => 'email, user_birthday, read_stream', :display => 'popup'
 end
