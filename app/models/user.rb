3ac830df20590c6504526e9fa4795ca0798db4fa
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-01-27 12:30:26 +0530

    Added annotate gem, style the form, give basic styling to results table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  email              :string           default(""), not null
+#  encrypted_password :string           default(""), not null
+#  sign_in_count      :integer          default("0"), not null
+#  current_sign_in_at :datetime
+#  last_sign_in_at    :datetime
+#  current_sign_in_ip :string
+#  last_sign_in_ip    :string
+#  created_at         :datetime
+#  updated_at         :datetime
+#  name               :string
+#  gender             :string
+#  dob                :date
+#  provider           :string
+#  uid                :string
+#
+
 class User < ActiveRecord::Base
   
   has_many :ridings
--- test/models/riding_test.rb
+++ test/models/riding_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: ridings
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  ride_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class RidingTest < ActiveSupport::TestCase
