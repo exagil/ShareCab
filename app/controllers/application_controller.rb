0d5df3c9849ae55172fda5d57c08d9a7ddfaf7c0
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-01-28 16:11:49 +0530

    Set up callback. Authorize side bar in rides index to registered users. Redirect login from home page to facebook.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+  def after_sign_in_path_for(resource_or_scope)
+
+    if request.env['omniauth.origin']
+      if request.env['omniauth.origin']=='http://localhost:3000/'
+          request.env['omniauth.origin']='http://localhost:3000/static_pages/dashboard'
+      end
+      request.env['omniauth.origin']
+    end
+  end
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -30,11 +30,11 @@
 # IMPROVE IT
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
+    # store_location
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
