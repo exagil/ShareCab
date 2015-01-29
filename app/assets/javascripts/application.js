56473d111b4df8375474a1bfe47f5bfd2506d9d0
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-01-29 10:44:09 +0530

    Fixed the map load problem by removing turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,10 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery.geocomplete
 //= require geocomplete_static_pages
-//= require turbolinks
 //= require_tree .
