be91ba36f1fe36cab7d4cd2feae436e8bcc052b0
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-01-30 09:25:59 +0530

    Modified the add_initiater_to_ride to correct initiater references

--- db/migrate/20150124130811_add_initiater_to_ride.rb
+++ db/migrate/20150124130811_add_initiater_to_ride.rb
@@ -1,6 +1,5 @@
 class AddInitiaterToRide < ActiveRecord::Migration
   def change
     add_reference :rides, :initiater, index: true
-    add_foreign_key :rides, :initiaters
-  end
+    add_foreign_key :rides, :initiater
 end
