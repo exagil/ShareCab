ae3a7ba9e5c2c93a5a26bad89234b3f2f884c5ab
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-01-30 11:14:03 +0530

    Changed the config mailer constants

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,10 +86,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address => "email-smtp.us-east-1.amazonaws.com",
-      :user_name => SMTP_USERNAME, # Your SMTP user here.
-      :password => SMTP_PWD, # Your SMTP password here.
+      :user_name => ENV['SMTP_USERNAME'], # Your SMTP user here.
+      :password => ENV['SMTP_PWD'], # Your SMTP password here.
       :authentication => :login,
+      :domain => 'heroku.com',
       :enable_starttls_auto => true
   }
+
 
 end
