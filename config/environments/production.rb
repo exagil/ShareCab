ad102c62c5fe851572bd66cbce32dc0b36e451d6
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-01-30 11:30:46 +0530

    Change the smtp :address in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address => "email-smtp.us-east-1.amazonaws.com",
+      :address => "email-smtp.us-west-2.amazonaws.com",
       :user_name => ENV['SMTP_USERNAME'], # Your SMTP user here.
       :password => ENV['SMTP_PWD'], # Your SMTP password here.
       :authentication => :login,
