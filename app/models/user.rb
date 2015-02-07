526e40b700aaa83b7cce8a0222a6ce822bf66932
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-02-07 14:36:51 +0530

    Fetch user image from facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      # user.image = auth.info.image # assuming the user model has an image
+      user.image = auth.info.image # assuming the user model has an image
       user.dob = auth.extra.raw_info.user_birthday
       user.gender = auth.extra.raw_info.gender
       if bool==nil
