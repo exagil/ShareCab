98c35fd0168a861b6ad4501a4d2f3cfdd0d5440f
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2015-02-18 10:23:34 +0530

    Creates a blank map when clicked on date. Passes params to origin_lat and etc. on clicking date

--- app/assets/javascripts/geocomplete_static_pages.js
+++ app/assets/javascripts/geocomplete_static_pages.js
@@ -69,12 +69,98 @@ $(document).ready(function(){
     });
   }); // end of destination   
 
-
-  // Passing values to origin_lat and origin_lng
-  $("#find-pal").on('click', function(){
+  $("#date").on('click', function(){
+    //passing values to origin_lat, origin_lng when clicked on date
     $("#origin_lat").val($(".origin-lat").val());
     $("#origin_lng").val($(".origin-lng").val());
     $("#destination_lat").val($(".destination-lat").val());
     $("#destination_lng").val($(".destination-lng").val());
+
+    styles=
+
+[
+    {
+        "stylers": [
+            {
+                "hue": "#dd0d0d"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    }
+];
+
+
+
+      var directionsDisplay;
+      var directionsService = new google.maps.DirectionsService();
+      var map;
+      // var contentString = "Lorem ipsum";
+
+      // var infowindow = new google.maps.InfoWindow({
+      //   // content:contentString
+      // });
+
+      // var marker;
+      function initialize() {
+      map = new google.maps.Map(document.getElementById('map-canvas-destination')[0], {
+          zoom: 10,
+          // center: new google.maps.LatLng(-33.92, 151.25),
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+
+      map.setOptions({styles: styles});
+      
+      directionsDisplay = new google.maps.DirectionsRenderer();
+
+      directionsDisplay.setMap(map);
+
+      var start = new google.maps.LatLng($("#origin-lat").val(), $("#origin-lng").val());
+      var end = new google.maps.LatLng($("#destination-lat").val(), $("#destination-lng").val());
+      var request = {
+          origin: start,
+          destination: end,
+          travelMode: google.maps.DirectionsTravelMode.DRIVING
+      };
+      directionsService.route(request, function (response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+              directionsDisplay.setDirections(response);
+          }
+      });
+      }
+      // google.maps.event.addDomListener(window, 'load', initialize);
+      $('.map-canvas-destination').empty();
+      initialize();
+
+      })
+
+
+  // Passing values to origin_lat and origin_lng on submit
+  $("#find-pal").on('click', function(){
+    if ($('#origin_lat').is(':empty')){
+    $("#origin_lat").val($(".origin-lat").val());
+    $("#origin_lng").val($(".origin-lng").val());
+    $("#destination_lat").val($(".destination-lat").val());
+    $("#destination_lng").val($(".destination-lng").val());
+  }
   });
 });
