51b81050eb05186f1c94fb711aeb9306b849c1b9
Chirag Aggarwal <chi6rag@gmail.com>
2015-03-01 14:07:04 +0530

    Add .commits file; add apt ride/show statuses for rides; edit seed time to future; edit departure date look

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-ride = Ride.new(origin: "Delhi, New Delhi, India", origin_lat: 28.6139391, origin_long: 77.20902120000005, destination: "Hyderabad, Telangana, India", destination_lat: 17.385044, destination_long: 78.486671, departure_date: Date.today, departure_time: Time.now, number_of_seats: 3, number_of_seats_occupied:1, initiator_id:1)
+ride = Ride.new(origin: "Delhi, New Delhi, India", origin_lat: 28.6139391, origin_long: 77.20902120000005, destination: "Hyderabad, Telangana, India", destination_lat: 17.385044, destination_long: 78.486671, departure_date: Date.today, departure_time: Time.now+96000, number_of_seats: 3, number_of_seats_occupied:1, initiator_id:1)
 ride.save
 ride.ridings.create(user_id: 1)
 
@@ -17,7 +17,21 @@ ride = Ride.new(origin: "Pitampura, New Delhi, Delhi, India",
             destination_lat: 28.7164134,
             destination_long: 77.15458460000002,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
+            number_of_seats: 3,
+            number_of_seats_occupied:1,
+            initiator_id:1)
+ride.save
+ride.ridings.create(user_id: 1)
+
+ride = Ride.new(origin: "Pitampura, New Delhi, Delhi, India",
+            origin_lat: 28.69899839999999,
+            origin_long: 77.13841739999998,
+            destination: "Gurgaon, Haryana, India",
+            destination_lat: 28.4594965,
+            destination_long: 77.02663830000006,
+            departure_date: Date.today,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
@@ -38,6 +52,7 @@ ride = Ride.new(origin: "Pitampura, New Delhi, Delhi, India",
 ride.save
 ride.ridings.create(user_id: 1)
 
+
 ride = Ride.new(origin: "Gachibowli, Hyderabad, Telangana, India",
             origin_lat: 17.4400802,
             origin_long: 78.34891679999998,
@@ -45,7 +60,7 @@ ride = Ride.new(origin: "Gachibowli, Hyderabad, Telangana, India",
             destination_lat: 17.4325235,
             destination_long: 78.40701519999993,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
@@ -59,7 +74,7 @@ ride = Ride.new(origin: "Gachibowli, Hyderabad, Telangana, India",
             destination_lat: 31.1471305,
             destination_long: 75.34121789999995,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
@@ -73,7 +88,7 @@ ride = Ride.new(origin: "Gachibowli, Hyderabad, Telangana, India",
             destination_lat: 17.4141797,
             destination_long: 78.41169000000002,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
@@ -87,7 +102,7 @@ ride = Ride.new(origin: "Gachibowli, Hyderabad, Telangana, India",
             destination_lat: 17.240263,
             destination_long: 78.42938500000002,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
@@ -101,7 +116,7 @@ ride = Ride.new(origin: "Care Hospitals, Hyderabad, Telangana, India",
             destination_lat: 17.4325235,
             destination_long: 78.40701519999993,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
@@ -115,7 +130,7 @@ ride = Ride.new(origin: "Delhi, New Delhi, India",
             destination_lat: 28.5355161,
             destination_long: 77.39102649999995,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
@@ -129,7 +144,7 @@ ride = Ride.new(origin: "Delhi, New Delhi, India",
             destination_lat: 28.4089123,
             destination_long: 77.31778940000004,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
@@ -143,7 +158,7 @@ ride = Ride.new(origin: "Kangra, Himachal Pradesh, India",
             destination_lat: 18.5977841,
             destination_long: 73.81433649999997,
             departure_date: Date.today,
-            departure_time: Time.now,
+            departure_time: Time.now+96000,
             number_of_seats: 3,
             number_of_seats_occupied:1,
             initiator_id:1)
