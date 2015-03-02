24e391cf739f364d10a794d240bb8909415c3ca8
Chirag Aggarwal <chi6rag@gmail.com>
2015-03-02 10:03:34 +0530

    Resolve pull errors

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -50,7 +50,7 @@ class RidesController < ApplicationController
         session[:date] = nil
         session[:date_filter] = params[:date_filter]
       end
-      @rides = Ride.get_suitable_rides(5, session[:origin_lat],session[:origin_lng], session[:destination_lat],session[:destination_lng], (session[:date_filter] || session[:date]).to_date)
+      @rides = Ride.get_suitable_rides(2, session[:origin_lat],session[:origin_lng], session[:destination_lat],session[:destination_lng], (session[:date_filter] || session[:date]).to_date)
     end
     if(@rides.length<1) # no rides
       redirect_to static_pages_no_ride_found_url
